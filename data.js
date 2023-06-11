fe81f3a3f8b3f76fef42812a86fb57cab4f09b73
Raja287 <rajaofficial1621@gmail.com>
2023-06-11 10:16:15 +0200

    New

---
+++ data.js
@@ -0,0 +1,433 @@
+klynt.data = {
+    "mainSequence": "AA5BB4C5-84CF-22DD-3EF5-4E4C6799F3DB",
+    "advanced": {
+        "remoteVideosAPILocalBitrate": "best",
+        "hasRemoteVideos": false,
+        "enableKeyboardNavigation": true,
+        "enableFullWindowMode": true,
+        "enableScrollNavigation": true,
+        "enableSwipeNavigation": true,
+        "remoteVideosAPIURL": "http://api.klynt.net/video/klynt-video-api.php"
+    },
+    "sequences": {
+        "AA5BB4C5-84CF-22DD-3EF5-4E4C6799F3DB": {
+            "color": "rgba(0,0,0,1)",
+            "duration": 203.712,
+            "id": "AA5BB4C5-84CF-22DD-3EF5-4E4C6799F3DB",
+            "title": "Home",
+            "images": [
+                {
+                    "zIndex": 5,
+                    "horizontalAlign": "center",
+                    "src": "Medias/Photos/bottone_sono_pronto.png",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    },
+                    "dataend": "03:24.80",
+                    "click": [
+                        {
+                            "value": "0",
+                            "duration": 0.5,
+                            "type": "style",
+                            "name": "opacity",
+                            "reversible": false,
+                            "delay": 0,
+                            "easing": "linear",
+                            "target": "self"
+                        },
+                        {
+                            "reversible": false,
+                            "delay": 0,
+                            "type": "mediaAction",
+                            "name": "play",
+                            "target": "8E5774C7-69B3-3DCD-AE96-68392EA92C70"
+                        },
+                        {
+                            "value": "1",
+                            "duration": 0.5,
+                            "type": "style",
+                            "name": "opacity",
+                            "reversible": false,
+                            "delay": 0.5,
+                            "easing": "linear",
+                            "target": "self"
+                        }
+                    ],
+                    "height": "385.58",
+                    "scaleMode": "zoom",
+                    "link": {
+                        "hideArrowInMindmap": false,
+                        "automaticTransition": false,
+                        "targetSequence": "D8B9963D-37AA-81F9-3CC0-681C6EFB8D2E",
+                        "hideInMindmap": false,
+                        "linkType": "linkToSequence",
+                        "controlDX": 0,
+                        "transition": "fade",
+                        "controlDY": 0,
+                        "id": "B7B3FA48-9C1E-3BC3-5F90-6BD916FBFEED",
+                        "color": "rgba(0,0,0,1)"
+                    },
+                    "mediaWidth": 600,
+                    "scales": true,
+                    "duration": "03:03.64",
+                    "centerX": "9.45",
+                    "name": "bottone_sono_pronto.png",
+                    "width": "385.58",
+                    "mediaHeight": 600,
+                    "databegin": "00:21.16",
+                    "centerY": "199.47",
+                    "verticalAlign": "middle",
+                    "id": "0A3660B3-7C91-ACB1-C94E-6BD14195BB36"
+                }
+            ],
+            "x": 639.9833333333333,
+            "videos": [
+                {
+                    "zIndex": 2,
+                    "src": [
+                        {
+                            "type": "video/mp4",
+                            "src": "Medias/Videos/video_klynt_schermata_ufficiale_lalalala.mp4"
+                        }
+                    ],
+                    "frameRate": 0,
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    },
+                    "poster": "Medias/Posters/video_klynt_schermata_ufficiale_lalalala.mp4.jpg",
+                    "player": false,
+                    "loop": false,
+                    "scaleMode": "letterbox",
+                    "fitToWindow": true,
+                    "syncMaster": true,
+                    "duration": "03:23.71",
+                    "volume": 1,
+                    "name": "video_klynt_schermata_ufficiale_lalalala.mp4",
+                    "mediaWidth": 1920,
+                    "id": "59DB9D12-9D7F-F283-CB55-6BD52152F3B9",
+                    "mediaHeight": 1080,
+                    "databegin": "00:00.00",
+                    "autoplay": true,
+                    "dataend": "03:23.71"
+                }
+            ],
+            "size": "large",
+            "y": 390,
+            "backgroundColor": "rgba(0,0,0,1)",
+            "hideInMindmap": false,
+            "hideThumbnailInMindmap": false,
+            "hideTitleInMindmap": false,
+            "audios": [
+                {
+                    "left": "0.00",
+                    "src": [
+                        {
+                            "type": "audio/mp3",
+                            "src": "Medias/Audios/128918_ecfike_click__online-audio-converter.com_.mp3"
+                        }
+                    ],
+                    "autoplay": false,
+                    "dataend": "03:24.80",
+                    "zIndex": 3,
+                    "continuous": false,
+                    "sequenceEndVolume": 1,
+                    "mediaId": "7A49692B-487E-9F0D-A557-68327CAEFEEA",
+                    "overlayVolume": 1,
+                    "height": "30.00",
+                    "player": false,
+                    "scales": false,
+                    "duration": "03:03.64",
+                    "volume": 1,
+                    "name": "128918_ecfike_click__online-audio-converter.com_.mp3",
+                    "bottom": "-3.00",
+                    "databegin": "00:21.16",
+                    "right": "-0.00",
+                    "id": "8E5774C7-69B3-3DCD-AE96-68392EA92C70",
+                    "loop": false
+                }
+            ],
+            "thumbnail": "Medias/Thumbnails/schermata_1_klynt_ufficiale.mp4.jpg"
+        },
+        "D8B9963D-37AA-81F9-3CC0-681C6EFB8D2E": {
+            "color": "rgba(0,0,0,1)",
+            "duration": 14.61,
+            "shapes": [
+                {
+                    "zIndex": 4,
+                    "height": "46.51",
+                    "type": "default",
+                    "style": {
+                        "backgroundColor": "rgba(255,255,255,1)",
+                        "opacity": 0,
+                        "borderRadius": 5
+                    },
+                    "id": "D27FA3A5-0E79-542B-F3A8-6BE70D85404B",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    },
+                    "link": {
+                        "window": "_parent",
+                        "targetURL": "https://www.instagram.com/ariele.2150/",
+                        "linkType": "linkToURL",
+                        "id": "E7E35218-9137-5E0E-153F-A62C165B85C2"
+                    },
+                    "click": [
+                        {
+                            "value": "0.4",
+                            "duration": 0.5,
+                            "type": "style",
+                            "name": "opacity",
+                            "reversible": false,
+                            "delay": 0,
+                            "easing": "linear",
+                            "target": "self"
+                        },
+                        {
+                            "reversible": false,
+                            "delay": 0,
+                            "type": "mediaAction",
+                            "name": "play",
+                            "target": "6A61FC90-ED64-4E54-914D-6BEB4C31F7A0"
+                        },
+                        {
+                            "value": "0",
+                            "duration": 0.5,
+                            "type": "style",
+                            "name": "opacity",
+                            "reversible": false,
+                            "delay": 0.3,
+                            "easing": "linear",
+                            "target": "self"
+                        }
+                    ],
+                    "scales": true,
+                    "duration": "00:14.61",
+                    "centerX": "165.54",
+                    "width": "194.39",
+                    "databegin": "00:12.34",
+                    "centerY": "112.70",
+                    "label": "Hotspot 1",
+                    "dataend": "00:26.95"
+                }
+            ],
+            "id": "D8B9963D-37AA-81F9-3CC0-681C6EFB8D2E",
+            "title": "Sequence 1",
+            "x": 630.9833333333333,
+            "videos": [
+                {
+                    "zIndex": 3,
+                    "src": [
+                        {
+                            "type": "video/mp4",
+                            "src": "Medias/Videos/schermata_ig_klynt.mp4"
+                        }
+                    ],
+                    "frameRate": 0,
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    },
+                    "poster": "Medias/Posters/schermata_ig_klynt.mp4.jpg",
+                    "player": false,
+                    "loop": false,
+                    "scaleMode": "letterbox",
+                    "fitToWindow": true,
+                    "syncMaster": true,
+                    "duration": "00:14.61",
+                    "volume": 1,
+                    "name": "schermata_ig_klynt.mp4",
+                    "mediaWidth": 1920,
+                    "id": "CF185C9F-C238-23ED-426C-6BDF5FE60C1F",
+                    "mediaHeight": 1080,
+                    "databegin": "00:00.00",
+                    "autoplay": true,
+                    "dataend": "00:14.61"
+                }
+            ],
+            "size": "small",
+            "y": 171,
+            "backgroundColor": "rgba(0,0,0,0.75)",
+            "hideInMindmap": false,
+            "hideThumbnailInMindmap": false,
+            "hideTitleInMindmap": false,
+            "audios": [
+                {
+                    "left": "0.00",
+                    "src": [
+                        {
+                            "type": "audio/mp3",
+                            "src": "Medias/Audios/128918_ecfike_click__online-audio-converter.com_.mp3"
+                        }
+                    ],
+                    "autoplay": false,
+                    "dataend": "00:15.61",
+                    "zIndex": 2,
+                    "continuous": false,
+                    "sequenceEndVolume": 1,
+                    "mediaId": "7A49692B-487E-9F0D-A557-68327CAEFEEA",
+                    "overlayVolume": 1,
+                    "height": "30.00",
+                    "player": false,
+                    "scales": false,
+                    "duration": "00:03.27",
+                    "volume": 1,
+                    "name": "128918_ecfike_click__online-audio-converter.com_.mp3",
+                    "bottom": "0.00",
+                    "databegin": "00:12.34",
+                    "right": "0.00",
+                    "id": "6A61FC90-ED64-4E54-914D-6BEB4C31F7A0",
+                    "loop": false
+                }
+            ],
+            "thumbnail": "Medias/Thumbnails/schermata_ig_klynt.mp4.jpg"
+        }
+    },
+    "aliases": {
+        "aliasToId": {
+            "Home": "AA5BB4C5-84CF-22DD-3EF5-4E4C6799F3DB",
+            "Sequence_1": "D8B9963D-37AA-81F9-3CC0-681C6EFB8D2E"
+        },
+        "idToAlias": {
+            "AA5BB4C5-84CF-22DD-3EF5-4E4C6799F3DB": "Home",
+            "D8B9963D-37AA-81F9-3CC0-681C6EFB8D2E": "Sequence_1"
+        }
+    },
+    "wording": {
+        "program_percentage_viewed": "du programme vu",
+        "total_sequences": "séquences au total",
+        "your_are_here": "Vous êtes ici",
+        "keywords": "mots-clés",
+        "total_duration": "(durée totale : {0})"
+    },
+    "miniPlayer": {
+        "resumePlaybackWording": "Resume playback?",
+        "yesWording": "Yes",
+        "redirectToMobileApp": "auto",
+        "thanksForWatchingWording": "Thanks for watching!",
+        "noWording": "No",
+        "analyticsKey": "",
+        "launchAppWording": "Then Launch Project",
+        "title": "NUTOPIA_START",
+        "url": "",
+        "downloadAppWording": "Download App",
+        "fullscreenInfoWording": "This program will launch in fullscreen",
+        "thumbnail": "Medias/Posters/schermata_1_klynt_ufficiale.mp4.jpg",
+        "description": ""
+    },
+    "general": {
+        "overlayCloseButtonRight": 16,
+        "analyticsKey": "",
+        "overlayCloseButtonTop": 16,
+        "modificationDate": 1686471355520,
+        "mapsKey": "",
+        "creationDate": 1684940809225,
+        "editorVersionLabel": "Klynt 3.5.007",
+        "title": "NUTOPIA_START",
+        "primaryColor": 3355443,
+        "editorVersionNumber": "3.5.007",
+        "username": "polito",
+        "secondaryColor": 16777215,
+        "author": "polito",
+        "tertiaryColor": 15286861,
+        "mainFontName": "Open Sans",
+        "ratio": "16/9",
+        "keywords": null,
+        "mainFontSize": 14,
+        "width": 970,
+        "height": 545,
+        "level": 5,
+        "projectId": "4A821C8E-B111-E50C-BD9C-4E4C6409FDB3",
+        "projectHost": "",
+        "description": ""
+    },
+    "watermark": {
+        "displayOnStartSequence": true,
+        "position": "top-left",
+        "image": "Player/css/editor/img/watermark.png",
+        "enabled": false,
+        "action": {
+            "type": "openSequence",
+            "target": null
+        }
+    },
+    "share": {
+        "enableTumblr": true,
+        "message": "",
+        "customSizeWording": "Custom",
+        "link": null,
+        "playerWording": "Player",
+        "miniPlayerWording": "Mini player",
+        "title": "",
+        "shortMessage": "",
+        "horizontalWording": "Horizontal",
+        "shareSequenceWording": "Share sequence",
+        "linkWording": "Link",
+        "verticalWording": "Vertical",
+        "socialNetworksWording": "Social Networks",
+        "removeViaKlynt": true,
+        "embedWording": "Embed",
+        "displayMiniPlayerWording": true,
+        "enableGooglePlus": true,
+        "originalSizeWording": "Original",
+        "enableLinkedIn": true,
+        "thumbnail": null
+    },
+    "menu": {
+        "backgroundImage": null,
+        "offset_sequence": 12,
+        "displayBackgroundImage": false,
+        "widgets": [
+            {
+                "params": {
+                    "sequences": [
+                        "AA5BB4C5-84CF-22DD-3EF5-4E4C6799F3DB",
+                        "D8B9963D-37AA-81F9-3CC0-681C6EFB8D2E"
+                    ],
+                    "displayDescriptions": true,
+                    "layout": "grid",
+                    "displayDuration": true
+                },
+                "type": "index",
+                "label": "Index",
+                "id": "F498A2C7-1FD5-4520-E345-4E4C67990CB7"
+            },
+            {
+                "params": {
+                    "creditsColumnTitle": "Credits",
+                    "creditsColumnText": "",
+                    "subtitle": "",
+                    "title": "NUTOPIA_START",
+                    "aboutColumnTitle": "About",
+                    "aboutColumnText": ""
+                },
+                "type": "credits",
+                "label": "Credits",
+                "id": "1F09A909-D69F-5ADB-B1A6-4E4C6799756D"
+            },
+            {
+                "placeHolderWording": "Type here...",
+                "type": "search",
+                "resultWording": "Result for: ",
+                "resultsWording": "Results for: ",
+                "enabled": true,
+                "label": "Search",
+                "id": "8D70C1AB-65BB-16DC-8E69-4E4C67993377"
+            },
+            {
+                "displayDescriptions": false,
+                "largeSequenceSize": 100,
+                "displayWatermark": false,
+                "mediumSequenceSize": 60,
+                "displayLinksArrow": true,
+                "smallSequenceSize": 25,
+                "type": "mindmap",
+                "label": "Mindmap",
+                "id": "18ACD829-828C-6B03-765D-4E4C6799A941"
+            }
+        ],
+        "searchWidget": "8D70C1AB-65BB-16DC-8E69-4E4C67993377"
+    }
+}
---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "resumePlaybackWording": "Resume playback?",
+    "yesWording": "Yes",
+    "redirectToMobileApp": "auto",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "noWording": "No",
+    "analyticsKey": "",
+    "launchAppWording": "Then Launch Project",
+    "title": "NUTOPIA_START",
+    "url": "",
+    "downloadAppWording": "Download App",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "thumbnail": "Medias/Posters/schermata_1_klynt_ufficiale.mp4.jpg",
+    "description": ""
+}
